fcecc1801b3b80c6e367d75012ca81ee01b1f37b
Jeff Nicholson <jeff.nicholson99@gmail.com>
2012-06-27 16:36:22 -0600

    Modify data gen script
    Group nodes more like in original

--- lib/csvToJson.py
+++ lib/csvToJson.py
@@ -1,13 +1,47 @@
 import json
+import string
+
+
+# level zero nodes
+fuelNamesWithMap = [	('Uranium',['urainium']), # note urainium is mispelles in current csv files
+							('Petroleum', ['crudeOil', 'refPetPrd', 'petroleum']),
+							('Coal',['coal']),
+							('Electricity', ['electricity']),
+							('NaturalGas/NGL',['naturalGas', 'NGL']),
+							('LiquidFuels',['hydrogen', 'otherLiqFuel']),
+							('Biomass/other',['biomass', 'geoSteam', 'wind', 'solar']),
+							('Hydro',['waterFall', 'waterTide', 'waterWave', 'inRiverFlow']) ]
+
+def findMappedNode(csvName):
+	for (node, list) in fuelNamesWithMap:
+		for l in list:
+			lLower = string.lower(l)
+			csvLower = string.lower(csvName)
+			if lLower == csvLower:
+				return node
+	print 'Could not find mapped node for %s' % csvName
+	return ""
+
+
 
 nodes = dict() 
-nodes['electricity'] = {'id':1, 'level':2}
 arcs = list() 
 
+# manually create the fuel nodes
+for (fuel,list) in fuelNamesWithMap:
+	nodes[fuel] = {'id':len(nodes), 'level':0}
+# add the electricity node
+nodes['Electricity']['level'] = 2
+# modify fuels that have no disposition data, they well be level 1
+nodes['Biomass/other']['level'] = 1 
+nodes['Hydro']['level'] = 1
 
+# read and interpret data, by assigning arcs between the fuel nodes and electricity nodes
+# there are two csv files with the elec gen data
 print "reading elecGenRenewablesUranium.csv..." 
 renewableElecCsv = open('elecGenRenewablesUranium.csv', 'r')
 
+# some crappy file verification
 line = renewableElecCsv.readline()
 assert line == 'entity: /year\n', "entity is not correct in elecGenRenewablesUranium.csv"
 line = renewableElecCsv.readline()
@@ -15,13 +49,37 @@ assert line == 'unit of measure: petajoule\n', "unit of measure is not correct"
 line = renewableElecCsv.readline()
 assert line.split(',')[0] == 'renewables', "first row of table should start with renewables"
 
+# read actual data
 for line in renewableElecCsv:
 	split = line.split(',')
-	name = split[0].strip('"')
-	nodes[name]={"id":len(nodes)+1, "level":0}
-	arcs.append({"srcid":len(nodes), "dstid":1, "flow":(float(split[1])/1000), "futureflow":(float(split[len(split)-1])/1000)})
+	csvName = split[0].strip('"')
+	# find correct node to assign data to
+	node = findMappedNode(csvName)
+	if node: 
+		arcs.append({"srcid":nodes[node]['id'], "dstid":nodes['Electricity']['id'], "flow":(float(split[1])/1000), "futureflow":(float(split[len(split)-1])/1000)})
 
 renewableElecCsv.close()
+print "done"
+
+
+print 'reading fuelUsedElecGen.csv'
+fuelUsedElecGenCsv = open('fuelUsedElecGen.csv', 'r')
+
+line = fuelUsedElecGenCsv.readline()
+assert line == 'entity: /year\n', "entity is not correct"
+line = fuelUsedElecGenCsv.readline()
+assert line == 'unit of measure: petajoule\n', "unit of measure is not correct"
+line = fuelUsedElecGenCsv.readline()
+
+for line in fuelUsedElecGenCsv:
+	split = line.split(',')
+	csvName = split[0].strip('"')
+	# find correct node to assign data to
+	node = findMappedNode(csvName)
+	if node: 
+		arcs.append({"srcid":nodes[node]['id'], "dstid":nodes['Electricity']['id'], "flow":(float(split[1])/1000), "futureflow":(float(split[len(split)-1])/1000)})
+
+fuelUsedElecGenCsv.close()
 print "done"
 
 
@@ -40,13 +98,13 @@ for line in finalDemandCsv:
 	fuelName = split[1].strip('"')
 	if useName not in nodes:
 		nodes[useName] = {'id':len(nodes)+1, 'level':4}
-	if fuelName not in nodes:
-		nodes[fuelName] = {'id':len(nodes)+1, 'level':0}
-	arcs.append({ 'srcid':nodes.get(fuelName).get('id'), 'dstid':nodes.get(useName).get('id'), 'flow':(float(split[2])/1000), 'futureflow':(float(split[len(split)-1])/1000)})
-
+	srcNode = findMappedNode(fuelName)
+	if srcNode:
+		arcs.append({ 'srcid':nodes[srcNode]['id'], 'dstid':nodes[useName]['id'], 'flow':(float(split[2])/1000), 'futureflow':(float(split[len(split)-1])/1000)})
 
 finalDemandCsv.close()
 print "done"
+
 
 
 # format data for export to json
@@ -57,3 +115,9 @@ jsonFile = open('../assets/json/historicalData.json', 'w')
 json.dump(data, jsonFile, indent=3)
 jsonFile.close()
 print "historicalData.json successfully generated"
+
+
+
+
+
+
